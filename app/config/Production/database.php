29ca3d6f9236c56d1ad2ed097e80dead7bb76bf9
Amrithamani <amrimani91@gmail.com>
2014-12-17 09:45:58 -0500

    debug in production server

---
+++ app/config/Production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'calorietracker',
+            'username'  => 'root',
+            'password'  => '9Fol0EKiA0',
+        ),
+
+    ),
+
+);
